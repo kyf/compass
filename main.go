9ae5f4c7318678a3e993e7dba96da9887c131ca3
kyf <kyf456@163.com>
2015-11-22 22:00:05 +0800

    debug

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	//"bytes"
+	"github.com/dchest/captcha"
 	"github.com/go-martini/martini"
+	//"image"
+	//"image/jpeg"
+	"net/http"
+	//"time"
 )
 
 const (
@@ -12,6 +18,11 @@ func main() {
 	m := martini.Classic()
 	m.Get("/", func() string {
 		return "server is running ..."
+	})
+	m.Use(martini.Static("./static/"))
+	m.Get("/checkcode", func(r *http.Request, w http.ResponseWriter) {
+		//http.ServeContent(w, r, name, time.Now(), bytes.NewReader(buf.Bytes()))
+		captcha.WriteImage(w, captcha.New(), 110, 40)
 	})
 
 	m.RunOnAddr(SERVER_ADDR)
