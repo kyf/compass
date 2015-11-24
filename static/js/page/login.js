8742aa98753da0e4c413c18917c74448e09e919d
root <root@develocal>
2015-11-24 18:28:30 +0800

    debug

--- static/js/page/login.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	var checkcode = $('#checkcode');
-
-
-	checkcode.bind('click', function(){
-		var src = '/checkcode?';
-		$(this).attr('src', src + Math.random());
-	});
-});
