313da9c873eb917293812e4e3d0548e348c4e682
kyf456 <kyf456@163.com>
2015-11-30 14:49:45 +0800

    add lbs

--- service/lbs_service.go
+++ service/lbs_service.go
@@ -1,9 +1,23 @@
 package service
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
+
+var (
+	ak          string = "C17e4ac75ae027235a4db6a79ccbe3e2"
+	lbs_api_uri string = "http://api.map.baidu.com/location/ip?ak=%s&ip=%s&coor=bd09ll"
+)
+
+type lbsapi struct {
+	Address string `json:"address"`
+	Status  int    `json:"status"`
+}
 
 type lbs_is_china struct{}
 
@@ -12,7 +26,51 @@ func (lbs *lbs_is_china) String() string {
 }
 
 func (lbs *lbs_is_china) Handle(r *http.Request, w http.ResponseWriter, logger *log.Logger) {
-	w.Write([]byte("asdasdad"))
+	RemoteAddr := strings.Split(r.RemoteAddr, ":")
+	ip := RemoteAddr[0]
+	res, err := http.Get(fmt.Sprintf(lbs_api_uri, ak, ip))
+	if err != nil {
+		logger.Printf("invalid ip add [%s], err is %v", ip, err)
+		result, _ := format(4000, nil)
+		w.Write(result)
+		return
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		logger.Printf("invalid ip add [%s], err is %v", ip, err)
+		result, _ := format(4000, nil)
+		w.Write(result)
+		return
+	}
+
+	var jsonBody lbsapi
+	err = json.Unmarshal(body, &jsonBody)
+	if err != nil {
+		logger.Printf("invalid ip add [%s], err is %v", ip, err)
+		result, _ := format(4000, nil)
+		w.Write(result)
+		return
+	}
+
+	if jsonBody.Status == 0 {
+		var result []byte
+		if strings.EqualFold(jsonBody.Address[:2], "CN") {
+			result, _ = format(1000, nil)
+		} else {
+			result, _ = format(4000, nil)
+		}
+
+		w.Write(result)
+		return
+	} else {
+		logger.Printf("invalid ip add [%s], err is %s", ip, string(body))
+		result, _ := format(4000, nil)
+		w.Write(result)
+		return
+	}
+	result, _ := format(1000, nil)
+	w.Write(result)
 }
 
 func init() {
