e445bda92d40c7ccbac393df24ff59baefa02838
kyf456 <kyf456@163.com>
2015-11-30 16:19:48 +0800

    db

---
+++ data/dbhelper.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/go-sql-driver/mysql"
+)
+
+const (
+	DB_HOST string = ""
+	DB_PORT string = ""
+	DB_     string = ""
+	DB_HOST string = ""
+	DB_HOST string = ""
+	DB_HOST string = ""
+)
+
+func initDB() {
+
+}
--- sql/main.sql
+++ sql/main.sql
@@ -1,5 +1,30 @@
-create database compass
+drop database if exists `compass`;
+create database compass;
 use compass
 delimiter //
 
-drop table if exists ``
+drop table if exists `admin` //
+create table `admin`(
+	`id` int(11) primary key auto_increment comment 'primary key',
+	`admin_name` varchar(100) not null comment 'admin login name',
+	`admin_pwd` varchar(250) not null comment 'admin login password'
+)engine=InnoDB default charset=utf8 comment 'administrator login account'
+//
+
+drop table if exists `app` //
+create table `app`(
+	`id` int(11) primary key auto_increment comment 'primary key',
+	`name` varchar(250) not null comment 'app name',
+	`icon` varchar(250) not null default '' comment 'app icon url',
+	`desc` text comment 'app description'
+)engine=InnoDB default charset=utf8 comment 'app main table'
+//
+
+drop table if exists `setting` //
+create table `setting`(
+	`id` int(11) primary key auto_increment comment 'primary key',
+	`ad_show` tinyint(1) not null default 0 comment 'whether show ad'
+)engine=InnoDB default charset=utf8 comment 'app setting table'
+//
+
+delimiter ;
